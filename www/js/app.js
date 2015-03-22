c2239d7c98dfaae4ff5ee8d738e9756465cfb896
Pat Riley <pat@bitpay.com>
2015-03-22 08:53:22 -0400

    sunday morning commit

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,13 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', [
+  'ionic', 
+  'starter.controllers', 
+  'starter.services',
+  'auth0',
+  'angular-storage','angular-jwt'
+])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
